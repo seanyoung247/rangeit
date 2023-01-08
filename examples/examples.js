d1d4df91353296226dd0fb3e94fa876e9a66d24a
sean <seanyoung247@gmail.com>
2023-01-08 03:12:20 +0000

    refactor: Removes no parameter range examples

--- examples/examples.js
+++ examples/examples.js
@@ -1,18 +1,9 @@
 import {Range, range} from "../range.js";
 import {example} from "./utils.js";
 
+(()=>{
 
 const rng = range(0, 10, 2);
-
-
-example(
-    'Creating a range with no parameters:',
-
-    ()=>{
-        const rng = new Range();
-        return rng.size > 0
-    }
-);
 
 
 example(
@@ -49,12 +40,11 @@ example(
     'Creating a range with helper function:',
 
     ()=>{
-        const rng1 = range();
-        const rng2 = range(5);
-        const rng3 = range(1, 5);
-        const rng4 = range(1, 5, 2);
+        const rng1 = range(5);
+        const rng2 = range(1, 5);
+        const rng3 = range(1, 5, 2);
 
-        return (rng1.size > 0) && (rng2.size === 6) && (rng3.size === 5) && (rng4.size === 3);
+        return (rng1.size === 6) && (rng2.size === 5) && (rng3.size === 3);
     }
 );
 
@@ -153,3 +143,5 @@ example(
         );
     }
 )
+
+})();
