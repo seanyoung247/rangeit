2b86a2f043d7207b0a7a84949cd640a8a03f2c7e
sean <seanyoung247@gmail.com>
2023-01-08 02:05:45 +0000

    test: Adds range helper function test

--- __tests__/range.test.js
+++ __tests__/range.test.js
@@ -1,4 +1,5 @@
-import Range from "../range.js";
+
+import {Range, range} from "../range.js";
 
 describe('Range class', () => {
 
@@ -27,13 +28,11 @@ describe('Range class', () => {
             const comp = [10,9,8,7,6,5,4,3,2,1];
             expect([...rng]).toEqual(comp);
         });
-
         test('Floating point ranges', () => {
             const rng = new Range(0.5, 0.55, 0.01);
             const comp = [0.5,0.51,0.52,0.53,0.54,0.55];
             expect([...rng]).toEqual(comp);
         });
-
         test('Handles ranges with no valid steps', () => {
             const rng = new Range(10,1,1);
             const comp = [];
@@ -42,6 +41,38 @@ describe('Range class', () => {
         test('Throws error on incorrect parameters', () => {
             expect(() => new Range(1, 10, '2')).toThrow(TypeError);
             expect(() => new Range(1, {}, 2)).toThrow('All range inputs must be valid numbers');
+        });
+
+        test('Helper function creates correct ranges', () => {
+            let rng = range();
+            expect(rng.size).toBeGreaterThan(0);
+
+            rng = range(5);
+            let comp = [0,1,2,3,4,5];
+            expect([...rng]).toEqual(comp);
+
+            rng = range(1, 5);
+            comp = [1,2,3,4,5];
+            expect([...rng]).toEqual(comp);
+
+            rng = range(1, 10, 2);
+            comp = [1,3,5,7,9];
+            expect([...rng]).toEqual(comp);
+
+            rng = range(10, 1, -1);
+            comp = [10,9,8,7,6,5,4,3,2,1];
+            expect([...rng]).toEqual(comp);
+
+            rng = range(0.5, 0.55, 0.01);
+            comp = [0.5,0.51,0.52,0.53,0.54,0.55];
+            expect([...rng]).toEqual(comp);
+
+            rng = range(10,1,1);
+            comp = [];
+            expect([...rng]).toEqual(comp);
+
+            expect(() => range(1, 10, '2')).toThrow(TypeError);
+            expect(() => range(1, {}, 2)).toThrow('All range inputs must be valid numbers');
         });
     });
 
