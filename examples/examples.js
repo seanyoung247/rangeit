19a41d60d20e64583a84270a564850ccd9d2c354
sean <seanyoung247@gmail.com>
2023-01-08 01:02:30 +0000

    feat: Adds range creation examples

--- examples/examples.js
+++ examples/examples.js
@@ -1,4 +1,62 @@
-import Range from "../range.js";
+import Range, {range} from "../range.js";
+
+
+const rng = range(0, 10, 2);
+
+
+example(
+    'Creating a range with no parameters:',
+
+    () => {
+        const rng = new Range();
+        return rng.size > 0
+    }
+);
+
+
+example(
+    'Creating a range with one parameter:',
+
+    () => {
+        const rng = new Range(5);
+        return rng.size === 6;
+    }
+);
+
+
+example(
+    'Creating a range with two parameters:',
+
+    () => {
+        const rng = new Range(1, 5);
+        return rng.size === 5;
+    }
+)
+
+
+example(
+    'Creating a range with three parameters:',
+    
+    () => {
+        const rng = new Range(1, 5, 2);
+        return rng.size === 3;
+    }
+)
+
+
+example(
+    'Creating a range with helper function:',
+
+    () => {
+        const rng1 = range();
+        const rng2 = range(5);
+        const rng3 = range(1, 5);
+        const rng4 = range(1, 5, 2);
+
+        return (rng1.size > 0) && (rng2.size === 6) && (rng3.size === 5) && (rng4.size === 3);
+    }
+);
+
 
 
 example(
